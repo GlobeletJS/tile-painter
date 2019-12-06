71835dd33df5eabe334e247813137e7992632050
Jeshurun Hembd <jjhembd@gmail.com>
2019-12-06 12:16:21 -0600

    Simplify example using vector-tile-esm

--- examples/klokan-basic/main.js
+++ examples/klokan-basic/main.js
@@ -1,6 +1,8 @@
 import { parseStyle  } from 'tile-stencil';
 import { addPainters } from "../../src/index.js";
-import { getTile     } from "./read.js";
+import { xhrPromise  } from "./xhr-promise.js";
+import { VectorTile  } from 'vector-tile-esm';
+import Protobuf from 'pbf';
 
 const styleHref = "./klokantech-basic-style.json";
 const tileHref = "./maptiler_11-327-791.pbf";
@@ -19,12 +21,25 @@ export function main() {
     .then( addPainters );
 
   // Load a tile, parse to GeoJSON
-  let tilePromise = getTile(tileHref, tileSize);
-  
+  let getTile = xhrPromise(tileHref, "arraybuffer")
+    .then( buffer => new VectorTile(new Protobuf(buffer)) )
+    .then( mvt => mvtToJSON(mvt, tileSize) );
+
   // Render to the Canvas
-  let render = Promise.all([getStyle, tilePromise])
+  let render = Promise.all([getStyle, getTile])
     .then( ([style, tile]) => {
       let sources = { openmaptiles: tile };
       style.layers.forEach( layer => layer.painter(ctx, 11, sources, bboxes) );
     });
+}
+
+function mvtToJSON(tile, size) {
+  // tile.laers is an object (not array!). In Mapbox Streets, it is an object
+  // of { name: layer } pairs, where name = layer.name. But this is not
+  // mentioned in the spec! So we use layer.name for safety
+  const jsonLayers = {};
+  Object.values(tile.layers).forEach(layer => {
+    jsonLayers[layer.name] = layer.toGeoJSON(size);
+  });
+  return jsonLayers;
 }
